ddb3fbfd5e911b259f6551594bd886fe0488fb73
Elisée Maurer <elisee@sparklinlabs.com>
2020-06-05 04:14:58 +0200

    Serve public data

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+app.use(express.static(path.resolve(__dirname, "../public")));
 app.use("/three.min.js", express.static(path.resolve(__dirname, "../node_modules/three/build/three.min.js")));
 
 io.on("connect", (socket) => {
